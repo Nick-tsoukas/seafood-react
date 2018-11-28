85fb926779ca65bc2dc39fc32008617eefea7116
Nick Tsoukas <nick.tsoukas101@gmail.com>
2018-11-27 19:02:11 -0700

    Building storepicker component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,16 +1,25 @@
-import React, { Fragment } from "react";
+import React from "react";
 import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+
+  goToStore = (event) => {
+   event.preventDefault();
+   //gets input
+   const storeName = this.myInput.value.value;
+   //changes route 
+   this.props.history.push(`/store/${storeName}`)
+  }
+  
   render() {
     return (
-      <Fragment>
-        <form action="" className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore} >
           <h2>Please Enter Store</h2>
-          <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+          <input type="text" ref={this.myInput} required placeholder="Store Name" defaultValue={getFunName()} />
           <button type="submit">Vist Store</button>
         </form>
-      </Fragment>
     );
   }
 }
